cf0408d9b0cdac433e48e100e89739fe7db33f19
edernclemente <53342699+EdernClemente@users.noreply.github.com>
2022-11-28 14:17:28 +0100

    Delete alloallo directory

--- alloallo/js/mobile-nav-script.js
+++
@@ -1,9 +0,0 @@
-
-function mobileNavOpen() {
-  var x = document.getElementById("openMobileNav");
-  if (x.className === "mobile_nav_hidden") {
-    x.className = "mobile_nav";
-  } else {
-    x.className = "mobile_nav_hidden";
-  }
-}
